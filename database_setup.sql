c03d93ba0bfef8a9b3de5e26ada5a7bd66a35845
barrettg21 <bg67483@uga.edu>
2025-11-29 22:53:59 -0500

    Set up database schema for workout application

--- database_setup.sql
+++ database_setup.sql
@@ -1,12 +1,59 @@
-drop database if exists cs4370_term_project_db;
-create database if not exists cs4370_term_project_db;
+drop database if exists workout_app;
+create database if not exists workout_app;
+use workout_app;
 
-use cs4370_term_project_db;
-
-create table user (
+-- USERS TABLE
+create table if not exists users (
     id int auto_increment,
-    username varchar(255) not null,
+    email varchar(255) not null,
+    uname varchar(255) not null,
     password varchar(255) not null,
-    primary key (id),
-    unique (username)
-)
+    created_at timestamp default current_timestamp,
+    primary key(id),
+    unique(email),
+    unique(uname)
+);
+
+-- WORKOUTS TABLE
+create table if not exists workouts (
+    id int auto_increment,
+    name varchar(255) not null,
+    user_id int not null,
+    description text,
+    startTime datetime,
+    endTime datetime,
+    created_at timestamp default current_timestamp,
+    primary key(id),
+    foreign key(user_id) references users(id) on delete cascade
+);
+
+-- EXERCISES TABLE
+create table if not exists exercises (
+    id int auto_increment,
+    name varchar(255) not null,
+    target_muscle varchar(255),
+    description text,
+    primary key(id)
+);
+
+-- WORKOUT_EXERCISES TABLE
+create table if not exists workout_exercises (
+    workout_id int not null,
+    exercise_id int not null,
+    time int,
+    sets int,
+    reps int,
+    primary key (workout_id, exercise_id),
+    foreign key (workout_id) references workouts(id) on delete cascade,
+    foreign key (exercise_id) references exercises(id) on delete cascade
+);
+
+-- GOALS TABLE
+create table if not exists goals (
+    id int auto_increment,
+    description varchar(255) not null,
+    user_id int not null,
+    created_at timestamp default current_timestamp,
+    primary key(id),
+    foreign key (user_id) references users(id) on delete cascade
+);
